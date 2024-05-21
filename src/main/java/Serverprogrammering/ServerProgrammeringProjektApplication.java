1d0722f2a1d3a008e86b58a9c8713b4e0c5b6278
Gurdhi0115 <150341211+Gurdhi0115@users.noreply.github.com>
2024-05-21 16:50:43 +0200

    adding gradle

---
+++ src/main/java/Serverprogrammering/ServerProgrammeringProjektApplication.java
@@ -0,0 +1,13 @@
+package Serverprogrammering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerProgrammeringProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerProgrammeringProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Serverprogrammering/ServerProgrammeringProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package Serverprogrammering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerProgrammeringProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
