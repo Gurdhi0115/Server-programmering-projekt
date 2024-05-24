e321c387c9309a3d92db193a310e3b2c6f4a8c19
Gurdhi0115 <150341211+Gurdhi0115@users.noreply.github.com>
2024-05-24 11:54:53 +0200

    deleting

--- Sererprogramming-projekt/src/main/java/se/yrgo/domain/Resporitory.java
+++
@@ -1,7 +0,0 @@
-package se.yrgo.domain;
-
-import se.yrgo.domain.User;
-import  org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Resporitory extends JPAresporitory <User, long> {
-}
