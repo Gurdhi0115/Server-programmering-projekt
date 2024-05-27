823a7586a40ba9e41b5968ebd87798b9f8e3b532
EliasSan <saile.dragednas@proton.me>
2024-05-27 16:36:23 +0200

    Loans class complete

--- Sererprogramming-projekt/src/main/java/se/yrgo/Serverprogramming/projekt/Loans.java
+++ Sererprogramming-projekt/src/main/java/se/yrgo/Serverprogramming/projekt/Loans.java
@@ -1,4 +1,67 @@
 package se.yrgo.Serverprogramming.projekt;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+
+@Entity
 public class Loans {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private Book book;
+
+    @ManyToOne
+    private Borrower borrower;
+
+    private LocalDate loanDate;
+    private LocalDate returnDate;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Borrower getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(Borrower borrower) {
+        this.borrower = borrower;
+    }
+
+    public LocalDate getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(LocalDate loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
 }
