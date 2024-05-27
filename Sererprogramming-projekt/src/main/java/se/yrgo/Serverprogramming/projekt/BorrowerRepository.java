1fc472074ad83639cf90ad7c3abdeff03f6554f2
EliasSan <saile.dragednas@proton.me>
2024-05-27 17:38:04 +0200

    removing Borrower

--- Sererprogramming-projekt/src/main/java/se/yrgo/Serverprogramming/projekt/BorrowerRepository.java
+++
@@ -1,6 +0,0 @@
-package se.yrgo.Serverprogramming.projekt;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BorrowerRepository extends JpaRepository<Borrower, Long> {
-}
