4c1cc0bf9c7963a74aca16b27217691f62b26ec7
EliasSan <saile.dragednas@proton.me>
2024-05-27 17:10:55 +0200

    adding LoansRepository

---
+++ Sererprogramming-projekt/src/main/java/se/yrgo/Serverprogramming/projekt/LoansRepository.java
@@ -0,0 +1,7 @@
+package se.yrgo.Serverprogramming.projekt;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoansRepository extends JpaRepository<Loans, Long> {
+
+}
